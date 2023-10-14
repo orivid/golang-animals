509089d158e61545f3da9cc58b1c294788209732
ori vider <or197@hotmail.com>
2023-10-14 16:07:01 +0300

    add puppy

---
+++ dog/puppy.go
@@ -0,0 +1,11 @@
+package dog
+
+import (
+	"github.com/GoesToEleven/dog"
+)
+
+func BigBark() string {
+
+	bark := "Woof"
+	return dog.WhenGrownUp(bark)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/orivid/golang-animals
 
 go 1.21.3
+
+require github.com/GoesToEleven/dog v0.0.0-20230428023317-90bef1c76cb9
